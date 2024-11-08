787ab6b7de23bd76a9692bfa9733aa3705572524
Leomendoza13 <leo.mendoza@epita.fr>
2024-11-08 15:24:14 +0100

    vpc.tf added

---
+++ terraform/vpc.tf
@@ -0,0 +1,39 @@
+resource "google_compute_network" "reddit_vpc" {
+  name                    = "reddit-feelings-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "reddit_subnet" {
+  name          = "reddit-subnet"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "europe-west9"
+  network       = google_compute_network.reddit_vpc.id
+}
+
+resource "google_compute_firewall" "allow_internal" {
+  name    = "allow-internal-traffic"
+  network = google_compute_network.reddit_vpc.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  source_ranges = ["10.0.0.0/16"]
+  direction     = "INGRESS"
+  target_tags   = ["spark"]
+}
+
+resource "google_compute_firewall" "allow_kafka_external" {
+  name    = "allow-kafka-external"
+  network = google_compute_network.reddit_vpc.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9092"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  direction     = "INGRESS"
+  target_tags   = ["kafka"]
+}
