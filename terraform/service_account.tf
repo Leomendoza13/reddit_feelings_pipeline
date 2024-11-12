cdd4d0a64fcc9be04d3d5fdf180390f75185b1cc
Leomendoza13 <leo.mendoza@epita.fr>
2024-11-12 12:35:09 +0100

    service_account.tf

--- terraform/service_account.tf
+++ terraform/service_account.tf
@@ -1,27 +1,16 @@
-resource "google_service_account" "kafka_service_account" {
-    account_id = "kafka_service_account"
-    display_name = "Kafka Service Account"
-}
-
 resource "google_service_account" "spark_service_account" {
-    account_id = "Spark_service_account"
+    account_id = "spark-service-account"
     display_name = "Spark Service Account"
 }
 
-resource "google_project_iam_member" "kafka_service_storage_object_admin" {
-  project = :"reddit-feelings-pipeline"
-  role    = "roles/storage.objectAdmin"
-  member  = "serviceAccount:${google_service_account.kafka_service_account.email}"
-}
-
 resource "google_project_iam_member" "spark_service_storage_object_admin" {
-  project = :"reddit-feelings-pipeline"
+  project = "reddit-feelings-pipeline"
   role    = "roles/storage.objectAdmin"
   member  = "serviceAccount:${google_service_account.spark_service_account.email}"
 }
 
-resource "google_project_iam_member" "spark_service_storage_object_admin" {
-  project = :"reddit-feelings-pipeline"
+resource "google_project_iam_member" "spark_service_biqquery_admin" {
+  project = "reddit-feelings-pipeline"
   role    = "roles/bigquery.admin"
   member  = "serviceAccount:${google_service_account.spark_service_account.email}"
 }
