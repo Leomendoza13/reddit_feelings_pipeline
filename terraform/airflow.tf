43af614bc6de87eca02fc225313e459406d2f926
Leomendoza13 <leo.mendoza@epita.fr>
2024-11-18 15:58:40 +0100

    airflow.tf updated

--- terraform/airflow.tf
+++ terraform/airflow.tf
@@ -6,7 +6,7 @@ resource "google_compute_instance" "airflow_vm" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
     }
   }
 
@@ -15,11 +15,38 @@ resource "google_compute_instance" "airflow_vm" {
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${file(var.ssh_pub_key_path)}"
+  }
+
   network_interface {
     network = google_compute_network.reddit_vpc.id
     subnetwork = google_compute_subnetwork.reddit_subnet.id
-    access_config {}
+
+    access_config {
+
+    }
   }
 
+  metadata_startup_script = file("${path.module}/scripts/airflow_script.sh")
+}
+
+resource "null_resource" "create_dags_dir" {
+  depends_on = [google_compute_instance.airflow_vm]
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mkdir -p /opt/airflow/dags /opt/airflow/config",
+      "sudo chmod -R 755 /opt/airflow/dags /opt/airflow/config",
+      "sudo chown -R ${var.ssh_user}:${var.ssh_user} /opt/airflow/dags /opt/airflow/config"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = var.ssh_user
+      private_key = file(replace(var.ssh_pub_key_path, ".pub", ""))
+      host        = google_compute_instance.airflow_vm.network_interface[0].access_config[0].nat_ip
+    }
+  }
 }
 
