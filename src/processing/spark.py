6bf27140c54026d9ef5b1198887d5ef48305bac0
Leomendoza13 <leo.mendoza@epita.fr>
2025-03-25 11:53:25 +0100

    black

--- src/processing/spark.py
+++ src/processing/spark.py
@@ -1,23 +1,34 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, TimestampType, IntegerType, ArrayType, FloatType
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    TimestampType,
+    IntegerType,
+    ArrayType,
+    FloatType,
+)
 from pyspark.sql.functions import from_unixtime, col, explode, current_timestamp, udf
 from transformers import pipeline
 
-spark = SparkSession.builder \
-    .appName("feeling analysis") \
-    .config("spark.jars", "/opt/bitnami/spark/jars/gcs-connector-hadoop3-2.2.11.jar") \
-    .config("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem") \
-    .config("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS") \
+spark = (
+    SparkSession.builder.appName("feeling analysis")
+    .config("spark.jars", "/opt/bitnami/spark/jars/gcs-connector-hadoop3-2.2.11.jar")
+    .config("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+    .config(
+        "fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS"
+    )
     .getOrCreate()
+)
+
 
 @udf(StringType(), useArrow=True)
 def analyze_sentiment(text):
 
     sentiment_analyzer = pipeline(
-    task='sentiment-analysis',
-    model='distilbert-base-uncased-finetuned-sst-2-english',
-    device=-1
-
+        task="sentiment-analysis",
+        model="distilbert-base-uncased-finetuned-sst-2-english",
+        device=-1,
     )
     if not text:
         return "neutral"
@@ -25,12 +36,12 @@ def analyze_sentiment(text):
         if len(text) > 512:
             print(f"Attention: Texte tronqué de {len(text)} à 512 caractères")
         result = sentiment_analyzer(text[:512])[0]
-        if result['label'] == 'POSITIVE':
-            if result['score'] > 0.8:
+        if result["label"] == "POSITIVE":
+            if result["score"] > 0.8:
                 return "très positif"
             return "positif"
-        elif result['label'] == 'NEGATIVE':
-            if result['score'] > 0.8:
+        elif result["label"] == "NEGATIVE":
+            if result["score"] > 0.8:
                 return "très négatif"
             return "négatif"
         return "neutre"
@@ -38,55 +49,63 @@ def analyze_sentiment(text):
         print(f"Erreur lors de l'analyse: {str(e)}")
         return "neutre"
 
-schema = StructType([
-    StructField("title", StringType(), True),
-    StructField("id", StringType(), True),
-    StructField("url", StringType(), True),
-    StructField("score", IntegerType(), True),
-    StructField("author", StringType(), True),
-    StructField("created_utc", FloatType(), True),
-    StructField("num_comments", IntegerType(), True),
-    StructField("selftext", StringType(), True),
-    StructField("subreddit", StringType(), True),
-    StructField("comments", ArrayType(StructType([
+
+schema = StructType(
+    [
+        StructField("title", StringType(), True),
         StructField("id", StringType(), True),
-        StructField("author", StringType(), True),
-        StructField("body", StringType(), True),
+        StructField("url", StringType(), True),
         StructField("score", IntegerType(), True),
+        StructField("author", StringType(), True),
         StructField("created_utc", FloatType(), True),
-        StructField("parent_id", StringType(), True),
-        StructField("is_submitter", StringType(), True)
-    ])), True)
-])
+        StructField("num_comments", IntegerType(), True),
+        StructField("selftext", StringType(), True),
+        StructField("subreddit", StringType(), True),
+        StructField(
+            "comments",
+            ArrayType(
+                StructType(
+                    [
+                        StructField("id", StringType(), True),
+                        StructField("author", StringType(), True),
+                        StructField("body", StringType(), True),
+                        StructField("score", IntegerType(), True),
+                        StructField("created_utc", FloatType(), True),
+                        StructField("parent_id", StringType(), True),
+                        StructField("is_submitter", StringType(), True),
+                    ]
+                )
+            ),
+            True,
+        ),
+    ]
+)
 
-streaming_df = spark.readStream \
-    .format("json") \
-    .schema(schema) \
-    .option("maxFilesPerTrigger", 1) \
-    .option("latestFirst", "true") \
-    .option("cleanSource", "archive") \
+streaming_df = (
+    spark.readStream.format("json")
+    .schema(schema)
+    .option("maxFilesPerTrigger", 1)
+    .option("latestFirst", "true")
+    .option("cleanSource", "archive")
     .load("gs://reddit-feelings-pipeline-bucket/*.json")
+)
 
-posts_df = streaming_df \
-    .select(
-        "id",
-        "title",
-        "url",
-        "score",
-        "author",
-        "num_comments",
-        "selftext",
-        "subreddit",
-        from_unixtime("created_utc").alias("post_date"),
-        current_timestamp().alias("processing_time")
-    )
+posts_df = streaming_df.select(
+    "id",
+    "title",
+    "url",
+    "score",
+    "author",
+    "num_comments",
+    "selftext",
+    "subreddit",
+    from_unixtime("created_utc").alias("post_date"),
+    current_timestamp().alias("processing_time"),
+)
 
 
-comments_df = streaming_df \
-    .select(
-        "id",
-        explode("comments").alias("comment")
-    ) \
+comments_df = (
+    streaming_df.select("id", explode("comments").alias("comment"))
     .select(
         col("id").alias("post_id"),
         col("comment.id").alias("comment_id"),
@@ -94,32 +113,35 @@ comments_df = streaming_df \
         col("comment.body").alias("comment_body"),
         col("comment.score").alias("comment_score"),
         from_unixtime("comment.created_utc").alias("comment_date"),
-        current_timestamp().alias("processing_time")
-    ) \
+        current_timestamp().alias("processing_time"),
+    )
     .withColumn("sentiment", analyze_sentiment(col("comment_body")))
+)
 
 project_id = "reddit-feelings-pipeline"
 dataset = "dataset"
 
-posts_query = posts_df.writeStream \
-    .format("bigquery") \
-    .option("table", f"{project_id}.{dataset}.posts") \
-    .option("checkpointLocation", "reddit-feelings-pipeline-process-bucket/posts") \
-    .option("failOnDataLoss", "false") \
-    .option("maxRetries", 3) \
-    .outputMode("append") \
-    .trigger(processingTime='1 minute') \
+posts_query = (
+    posts_df.writeStream.format("bigquery")
+    .option("table", f"{project_id}.{dataset}.posts")
+    .option("checkpointLocation", "reddit-feelings-pipeline-process-bucket/posts")
+    .option("failOnDataLoss", "false")
+    .option("maxRetries", 3)
+    .outputMode("append")
+    .trigger(processingTime="1 minute")
     .start()
+)
 
-comments_query = comments_df.writeStream \
-    .format("bigquery") \
-    .option("table", f"{project_id}.{dataset}.comments") \
-    .option("checkpointLocation", "reddit-feelings-pipeline-process-bucket/comments") \
-    .option("failOnDataLoss", "false") \
-    .option("maxRetries", 3) \
-    .outputMode("append") \
-    .trigger(processingTime='1 minute') \
+comments_query = (
+    comments_df.writeStream.format("bigquery")
+    .option("table", f"{project_id}.{dataset}.comments")
+    .option("checkpointLocation", "reddit-feelings-pipeline-process-bucket/comments")
+    .option("failOnDataLoss", "false")
+    .option("maxRetries", 3)
+    .outputMode("append")
+    .trigger(processingTime="1 minute")
     .start()
+)
 
 try:
     spark.streams.awaitAnyTermination()
